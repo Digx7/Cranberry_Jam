5330f5dd524cd3f0d25a898062d6e9f4725d6d10
Digx7 <37136301+Digx7@users.noreply.github.com>
2020-11-14 20:15:50 -0800

    Made some more progress

--- Assets/Scripts/AllEnemyManager.cs
+++ Assets/Scripts/AllEnemyManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AllEnemyManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] all01Enemys,
-                                          all02Enemys;
+                                          all02Enemys,
+                                          allEnemySpawning;
     [SerializeField] private GameObject codeHolder;
 
     public void DestoryAllEnemys(){
@@ -18,10 +19,15 @@ public class AllEnemyManager : MonoBehaviour
         if(enemy != null)
           enemy.GetComponent<SingleEnemyAiManager>().EnemyDie();
       }
+      foreach(GameObject enemy in allEnemySpawning){
+        if(enemy != null)
+          Destroy(enemy);
+      }
     }
 
     public void FindAllEnemys(){
       all01Enemys = GameObject.FindGameObjectsWithTag("Enemy 01");
       all02Enemys = GameObject.FindGameObjectsWithTag("Enemy 02");
+      allEnemySpawning = GameObject.FindGameObjectsWithTag("Enemy Spawning");
     }
 }
